2590211e106ae077b5cb2137ae05381a7240f11e
Mohammad Salah Akram Fuad <120177637+salahakramfuad@users.noreply.github.com>
2025-11-06 06:28:59 +0600

    Replace AlumniSection with placeholder text in Home component

--- app/achievements/alumni/page.jsx
+++ app/achievements/alumni/page.jsx
@@ -1,11 +1,7 @@
-import AlumniSection from 'components/Alumni'
-
 export default function Home() {
   return (
     <div>
-      <main>
-        <AlumniSection />
-      </main>
+      <main>Later</main>
     </div>
   )
 }
